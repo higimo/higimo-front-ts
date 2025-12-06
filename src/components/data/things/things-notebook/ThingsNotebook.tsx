919800e58aa7b9aa344a61e6300e729717b21d9d
Дмитрий Уткин <higimo@gmail.com>
2025-12-06 19:15:28 +0300

    Сокращение инфы о ноуте

--- src/components/data/things/things-notebook/ThingsNotebook.tsx
+++ src/components/data/things/things-notebook/ThingsNotebook.tsx
@@ -1,94 +1,36 @@
-export const ThingsNotebook = () => {
-	return (
-		<div className="content">
-			<h1>Ноутбук</h1>
-			<p>
-				Ультрабук ASUS ZenBook S UX391UA-ET084T
-			</p>
-			<table>
-				<tbody>
-					<tr>
-						<td>Операционная система</td>
-						<td>Windows 10 домашняя x64</td>
-					</tr>
-					<tr>
-						<td>Процессор</td>
-						<td>Intel Core i5 8250U 1.6 ГГц (max 3.4 ГГц) (4 ядра)</td>
-					</tr>
-					<tr>
-						<td>Графический контроллер</td>
-						<td>Intel UHD Graphics 620</td>
-					</tr>
-					<tr>
-						<td>Оперативная память (RAM)</td>
-						<td>8 ГБ (2133 МГц)</td>
-					</tr>
-					<tr>
-						<td>Объем SSD</td>
-						<td>512 ГБ</td>
-					</tr>
-					<tr>
-						<td>Диагональ и разрешение</td>
-						<td>13.3" 1920×1080 пикс. (IPS)</td>
-					</tr>
-					<tr>
-						<td>Звуковая система</td>
-						<td>Harman/Kardon</td>
-					</tr>
-					<tr>
-						<td>Поддержка Wi-Fi</td>
-						<td>a/b/g/n/ac</td>
-					</tr>
-					<tr>
-						<td>Bluetooth (версия)</td>
-						<td>4.2</td>
-					</tr>
-					<tr>
-						<td>Порт USB 3.1 тип C</td>
-						<td>3 шт</td>
-					</tr>
-					<tr>
-						<td>Разъем для наушников</td>
-						<td>1 шт. 3.5 мм</td>
-					</tr>
-					<tr>
-						<td>Датчик отпечатков пальцев</td>
-						<td>Да</td>
-					</tr>
-					<tr>
-						<td>Материал корпуса</td>
-						<td>магниевый сплав</td>
-					</tr>
-					<tr>
-						<td>Работа от аккумулятора</td>
-						<td>до 13 ч 30 мин</td>
-					</tr>
-					<tr>
-						<td>Цвет</td>
-						<td>темно-красный</td>
-					</tr>
-					<tr>
-						<td>Высота</td>
-						<td>13 мм</td>
-					</tr>
-					<tr>
-						<td>Ширина</td>
-						<td>311 мм</td>
-					</tr>
-					<tr>
-						<td>Глубина</td>
-						<td>213 мм</td>
-					</tr>
-					<tr>
-						<td>Вес</td>
-						<td>1.05 кг</td>
-					</tr>
-					<tr>
-						<td>Серия</td>
-						<td>ZenBook S</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	)
-}
+const NOTEBOOK_SPECS = [
+	['Операционная система', 'Windows 10 домашняя x64'],
+	['Процессор', 'Intel Core i5 8250U 1.6 ГГц (max 3.4 ГГц) (4 ядра)'],
+	['Графический контроллер', 'Intel UHD Graphics 620'],
+	['Оперативная память (RAM)', '8 ГБ (2133 МГц)'],
+	['Объем SSD', '512 ГБ'],
+	['Диагональ и разрешение', '13.3" 1920×1080 пикс. (IPS)'],
+	['Звуковая система', 'Harman/Kardon'],
+	['Поддержка Wi-Fi', 'a/b/g/n/ac'],
+	['Bluetooth (версия)', '4.2'],
+	['Порт USB 3.1 тип C', '3 шт'],
+	['Разъем для наушников', '1 шт. 3.5 мм'],
+	['Датчик отпечатков пальцев', 'Да'],
+	['Материал корпуса', 'магниевый сплав'],
+	['Работа от аккумулятора', 'до 13 ч 30 мин'],
+	['Цвет', 'темно-красный'],
+	['Высота', '13 мм'],
+	['Ширина', '311 мм'],
+	['Глубина', '213 мм'],
+	['Вес', '1.05 кг'],
+	['Серия', 'ZenBook S'],
+]
+
+export const ThingsNotebook = () => (
+	<div className="content">
+		<h1>Ноутбук</h1>
+		<p>Ультрабук ASUS ZenBook S UX391UA-ET084T</p>
+		<table>
+			<tbody>
+				{NOTEBOOK_SPECS.map(([label, value], i) => (
+					<tr key={i}><td>{label}</td><td>{value}</td></tr>
+				))}
+			</tbody>
+		</table>
+	</div>
+)
