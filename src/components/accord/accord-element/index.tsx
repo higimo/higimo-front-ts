57f879e02343e5ff1a61a5591049a2a9e51171c2
Дмитрий Уткин <higimo@gmail.com>
2025-12-06 13:52:36 +0300

    Аккорды. Вынес словарь тегов

--- src/components/accord/accord-element/index.tsx
+++ src/components/accord/accord-element/index.tsx
@@ -1,36 +1,50 @@
 import { FunctionComponent } from "preact"
 import { AccordModeType } from "../../../types"
-
-import { filterKey, filterMapping } from "../utils"
-
 import { ROUTE_LINKS } from 'dic/ROUTE_LINKS'
 import { Tag } from "components/ui/tag"
+import { filterMapping } from "../utils"
 
 let alf = ''
+
+const TAG_LABELS: Record<string, string> = {
+	liric: 'лирика',
+	scream: 'поорать',
+	korol: 'Король и шут',
+	funny: 'смешное',
+	rap: 'речитатив',
+	old: 'Старинное',
+	ussr: 'СССР',
+	lacky: 'зайдёт',
+	newschool: 'ньюскул',
+	bard: 'барды',
+}
+
 type AccordElementPropsType = Pick<AccordModeType, 'id' | 'name' | 'isMostView' | 'isNew' | 'view'> & {
 	showAlf?: boolean;
 	showBaidge?: boolean;
 }
-export const AccordElement: FunctionComponent<AccordElementPropsType> = ({ id, name, isMostView, isNew, view, showAlf = true, showBaidge = true }) => (
-	<span key={name}>
-		{showAlf ? ((name[0] !== alf) ? (<div className="alf">{alf = name[0]}</div>) : null) : null}
+
+export const AccordElement: FunctionComponent<AccordElementPropsType> = ({ 
+	id, 
+	name, 
+	isMostView, 
+	isNew, 
+	view, 
+	showAlf = true, 
+	showBaidge = true 
+}) => (
+	<span>
+		{showAlf && name[0] !== alf && <div className="alf">{alf = name[0]}</div>}
 		<div>
 			<a className="accord__link" href={ROUTE_LINKS.accordDetail({ idcode: id.toString() })}>{name}</a>
 			{' '}
-			{showBaidge ? [
+			{showBaidge && [
 				isMostView && <Tag>популярно ({view})</Tag>,
 				isNew && <Tag>нью</Tag>,
-				filterMapping[filterKey.liric]({ id }) && <Tag>лирика</Tag>,
-				filterMapping[filterKey.scream]({ id }) && <Tag>поорать</Tag>,
-				filterMapping[filterKey.korol]({ id }) && <Tag>Король и шут</Tag>,
-				filterMapping[filterKey.funny]({ id }) && <Tag>смешное</Tag>,
-				filterMapping[filterKey.rap]({ id }) && <Tag>речитатив</Tag>,
-				filterMapping[filterKey.old]({ id }) && <Tag>Старинное</Tag>,
-				filterMapping[filterKey.ussr]({ id }) && <Tag>СССР</Tag>,
-				filterMapping[filterKey.lacky]({ id }) && <Tag>зайдёт</Tag>,
-				filterMapping[filterKey.newschool]({ id }) && <Tag>ньюскул</Tag>,
-				filterMapping[filterKey.bard]({ id }) && <Tag>барды</Tag>,
-			].filter(Boolean).map(i => ([i, ' '])) : null}
+				...Object.entries(filterMapping).map(([key, fn]) => 
+					fn({ id }) && TAG_LABELS[key] && <Tag>{TAG_LABELS[key]}</Tag>
+				)
+			].filter(Boolean).flatMap(i => [i, ' '])}
 		</div>
 	</span>
 )
