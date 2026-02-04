889ff1a57257d5cca30660628d15afa694ff92d7
Дмитрий Уткин <higimo@gmail.com>
2026-02-04 10:45:01 +0300

    Рефакторинг useAuth

--- src/hook/use-auth.ts
+++ src/hook/use-auth.ts
@@ -1,81 +1,89 @@
-import { useEffect, useCallback, useContext, useState } from 'preact/hooks'
+import { useEffect, useCallback, useReducer } from 'preact/hooks'
 import { useLocation } from 'preact-iso'
 
 import sendRequest from 'utils/send-request'
-import { AuthContext } from 'context/auth'
 
 import { ROUTE_LINKS } from 'dic/ROUTE_LINKS'
 import { API_ROUTE } from 'dic/api-route'
 
 export const AUTH_STATUS_DIC = {
-	IDLE: 'idle',
-	LOADING: 'loading',
-	AUTHENTICATED: 'authenticated',
-	UNAUTHENTICATED: 'unauthenticated',
+	INIT:    'INIT',
+	LOADING: 'LOADING',
+	LOADED:  'LOADED',
+	ERROR:   'ERROR',
 } as const
 
 type AuthStatus = typeof AUTH_STATUS_DIC[keyof typeof AUTH_STATUS_DIC]
 
+interface AuthState {
+	status: AuthStatus
+	isAuth: boolean
+}
+
+type AuthAction =
+	| { type: 'INIT' }
+	| { type: 'LOADING' }
+	| { type: 'LOADED'; isAuth: boolean }
+	| { type: 'ERROR';  isAuth: boolean }
+
 /**
  * Замыкание, чтобы кешировать запрос
  */
-let authState: {
-	status: AuthStatus
-	isAuth: boolean
-} = {
-	status: 'idle',
+let cachedAuthState: AuthState = {
+	status: AUTH_STATUS_DIC.INIT,
 	isAuth: false,
 }
 
-type UseAuthType = () => {
+const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+	switch (action.type) {
+		case AUTH_STATUS_DIC.INIT:
+			return { ...state, status: 'INIT' }
+		case AUTH_STATUS_DIC.LOADING:
+			return { ...state, status: 'LOADING' }
+		case AUTH_STATUS_DIC.LOADED:
+			return { ...state, status: 'LOADED', isAuth: action.isAuth }
+		case AUTH_STATUS_DIC.ERROR:
+			return { ...state, status: 'LOADED', isAuth: action.isAuth }
+		default:
+			const exhaustiveCheck: never = action
+      		throw new Error(`Unhandled action type: ${exhaustiveCheck}`)
+	}
+}
+
+interface UseAuthReturn {
 	isAuth: boolean
 	isAuthLoading: boolean
 	redirectToLogin: () => void
 	routeTo: (url: string, replace?: boolean) => void
 }
-export const useAuth: UseAuthType = () => {
+export const useAuth = (): UseAuthReturn => {
 	const { route, path } = useLocation()
-
 	const redirectToLogin = useCallback(() => {
 		route(`${ROUTE_LINKS.login}?backpath=${path}`, true)
 	}, [path])
 
-	const { isAuth, setIsAuth } = useContext(AuthContext)
-	// только для обновления состояния наружу
-	const [ _, setStatusLoading ] = useState<AuthStatus>(authState.status)
+	const [state, dispatch] = useReducer(authReducer, cachedAuthState)
 
 	useEffect(() => {
-		if (authState.status === AUTH_STATUS_DIC.IDLE) {
-			authState.status = AUTH_STATUS_DIC.LOADING
-			setStatusLoading(authState.status)
+		if (state.status !== AUTH_STATUS_DIC.INIT) return
 
-			sendRequest(API_ROUTE.authMe)
-				.then(() => {
-					authState.isAuth = true
-					authState.status = AUTH_STATUS_DIC.AUTHENTICATED
+		cachedAuthState.status = AUTH_STATUS_DIC.LOADING
+		dispatch({ type: AUTH_STATUS_DIC.LOADING })
 
-					setIsAuth(authState.isAuth)
-					setStatusLoading(authState.status)
-				})
-				.catch(() => {
-					authState.isAuth = false
-					authState.status = AUTH_STATUS_DIC.UNAUTHENTICATED
-
-					setIsAuth(authState.isAuth)
-					setStatusLoading(authState.status)
-				})
-		} else {
-			setIsAuth(authState.isAuth)
-			setStatusLoading(authState.status)
-		}
-	}, [authState.status, setIsAuth, setStatusLoading])
+		sendRequest(API_ROUTE.authMe)
+			.then(() => {
+				cachedAuthState = { isAuth: true, status: AUTH_STATUS_DIC.LOADED}
+				dispatch({ type: AUTH_STATUS_DIC.LOADED, isAuth: true })
+			})
+			.catch(() => {
+				cachedAuthState = { isAuth: false, status: AUTH_STATUS_DIC.LOADED}
+				dispatch({ type: AUTH_STATUS_DIC.LOADED, isAuth: false })
+			})
+	}, [state, dispatch])
 
 	return {
-		isAuth,
-		isAuthLoading: [
-			AUTH_STATUS_DIC.AUTHENTICATED,
-			AUTH_STATUS_DIC.UNAUTHENTICATED
-		].includes(authState.status as any),
+		isAuth: state.isAuth,
+		isAuthLoading: state.status !== 'LOADED',
 		redirectToLogin,
 		routeTo: route,
 	}
