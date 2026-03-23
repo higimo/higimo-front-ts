5d7112a2546fec0be329a4bcf1f1bc9724127cbc
Дмитрий Уткин <higimo@gmail.com>
2026-03-23 14:07:14 +0300

    Вынес карту в отдельный компонент

---
+++ src/components/tourism/YandexMap/index.tsx
@@ -0,0 +1,40 @@
+import { useWindowSize } from "hook/use-window-size"
+import { YMaps, Map, FullscreenControl, Placemark } from "react-yandex-maps"
+import { barColor, barIcon } from "../tourism-maps-figure/data/bar-pov-moscow"
+
+// TODO: недо дубль TourismMapGeo, надо объединить
+export const YandexMap = ({ items }) => {
+	const { width, height } = useWindowSize()
+
+	return (
+		<div className="yandex-map">
+			<YMaps query={{ lang: 'ru_RU', apikey: '36482037-351d-458f-87ce-c2bf4a1e508b' }}>
+				<Map
+					width={Math.min(width * .85, 1200)}
+					height={Math.min(height * .6, 750)}
+					defaultState={{
+						zoom: 12,
+						center: [55.758772, 37.617933],
+					}}
+				>
+					<FullscreenControl />
+					{items.map((point) => (
+						<Placemark
+							modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+							geometry={point.coord}
+							properties={{
+								hintContent: point.title,
+								iconCaption: point.title,
+							}}
+							options={{
+								preset: barIcon(point.icon),
+								iconColor: barColor(point.color),
+								iconSize: [15, 15],
+							}}
+						/>
+					))}
+				</Map>
+			</YMaps>
+		</div>
+	)
+}
--- src/components/tourism/tourism-maps-moscow-bar/index.tsx
+++ src/components/tourism/tourism-maps-moscow-bar/index.tsx
@@ -1,21 +1,17 @@
-import { createRef, Fragment, FunctionComponent } from 'preact'
+import { Fragment } from 'preact'
 
 import { useEffect, useMemo, useState } from 'preact/hooks'
-import { useWindowSize } from 'hook/use-window-size'
-import { useTags } from 'hook/use-tags'
 
-import { YMaps, Map } from 'react-yandex-maps'
-import { TextContainer } from 'components/ui/text-container'
-import { Tag } from 'components/ui/tag'
 import { TourismBarPointSnippet } from 'components/tourism/tourism-bar-point-snippet'
 
 // TODO: пиздец, подключаю ленивую подгрузку, а вместе с типами всё равно гружу остальное
-import { BAR_COLOR_MAPPING, barColor, barIcon, BarPovType, barTagsCategory } from 'components/tourism/tourism-maps-figure/data/bar-pov-moscow'
+import { BarPovType, barTagsCategory } from 'components/tourism/tourism-maps-figure/data/bar-pov-moscow'
 
 import '../yandex-map.css'
 import { TagGroupedGallery } from '../TagGroupedGallery'
 import { TAG_GROUP_ALL_DISABLE, TAG_GROUP_ALL_ENABLE, useGroupTags } from 'hook/use-group-tags'
 import { filterTagAndGroupsStrategy } from 'utils/filter-tag-strategy/filterTagAndGroupsStrategy'
+import { YandexMap } from '../YandexMap'
 
 // TODO: Надо это уже в хэлпер унести, чтоб мне с этим не париться
 // TODO: А мне бы убрать, чтоб это не объект объектов был
@@ -24,41 +20,8 @@ const loadStateData = async (): Promise<{ barPovMoscow: BarPovType[] }> => {
 	return { barPovMoscow }
 }
 
-// TODO: типизировать функцию
-// TODO: map — это текущая карта, может сразу геообжект передавать?
-// TODO: yamaps — библиотека яндекса
-const updateMap = (map, yamaps, filteredData) => {
-	if (!map || !yamaps) {
-		console.log('skip', !map, !yamaps, !filteredData.lengt)
-		return null
-	}
-
-	map.geoObjects.removeAll()
-
-	filteredData
-		.forEach(mapPoint => {
-			map.geoObjects.add(new yamaps.Placemark(
-				mapPoint.coord,
-				{
-					hintContent: mapPoint.title,
-				},
-				{
-					preset: barIcon(mapPoint.icon),
-					iconColor: barColor(mapPoint.color),
-					iconSize: [40, 40],
-				}
-			))
-		})
-}
-
 export const TourismMapsMoscowBar = () => {
-	const refMap = createRef() // TODO: Унести в компонент карты
-	const [ yamaps, setYamaps ] = useState(null) // TODO: Унести в компонент карты
 	const [ stateData, setStateData ] = useState<{ barPovMoscow: BarPovType[] }>({ barPovMoscow: [] })
-
-	const { width, height } = useWindowSize() // TODO: Унести в компонент карты
-
-	const handleMapLoad = ymaps => setYamaps(ymaps) // TODO: Унести в компонент карты
 
 	const tagGroups = useMemo(() => {
 		return Object.keys(barTagsCategory).reduce((acc, categoryName) => {
@@ -91,10 +54,6 @@ export const TourismMapsMoscowBar = () => {
 		loadStateData().then(setStateData)
 	}, [])
 
-	useEffect(() => {
-		updateMap(refMap.current, yamaps, filteredData)
-	}, [refMap.current, yamaps, filteredData])
-
 	return (
 		<Fragment>
 			<TagGroupedGallery
@@ -106,30 +65,9 @@ export const TourismMapsMoscowBar = () => {
 				isAllSelected={isAllSelected}
 				isNoneSelected={isNoneSelected}
 			/>
-			{/* TODO: вынести в отдельный компонент, чтобы инкапсулировать width, height */}
-			<div className="yandex-map">
-				<YMaps query={{ lang: 'ru_RU' }}>
-					<Map
-						instanceRef={refMap}
-						onLoad={handleMapLoad}
-						width={Math.min(width * .85, 1200)}
-						height={Math.min(height * .6, 750)}
-						defaultState={{
-							zoom: 11,
-							center: [55.73, 37.75],
-						}}
-						modules={[
-							'Polygon',
-							'ObjectManager',
-							'GeoObject',
-							'Placemark',
-							'geoObject.addon.hint',
-							'ObjectManager', 'objectManager.addon.objectsBalloon'
-						]}
-					>
-					</Map>
-				</YMaps>
-			</div>
+			<YandexMap
+				items={filteredData}
+			/>
 			{filteredData && (
 				<div className="bar-pov__gallery">
 					{filteredData.map((mapPoint: BarPovType) => (
