ee2ca65141ea665e467d41f8d3fdd54b6a1afadd
Дмитрий Уткин <higimo@gmail.com>
2026-03-25 11:51:27 +0300

    Форматирование

--- src/dic/ROUTE_LINKS.ts
+++ src/dic/ROUTE_LINKS.ts
@@ -3,86 +3,88 @@ export const generateLink = <T extends string>(link: string) => (params: Record<
 	.reduce((carryLink, key) => carryLink.replace(`:${key}`, params[key]), link) as `/api/v2/${string}`
 
 const ROUTE_PROJECT_LINKS = Object.freeze({
-	projectIndex: '/project/',
-	projectTable: '/project/table',
-	projectTest: '/project/test',
-	projectSandbox: '/project/sandbox',
+	projectIndex:        '/project/',
+	projectTable:        '/project/table',
+	projectTest:         '/project/test',
+	projectSandbox:      '/project/sandbox',
 	projectDetail_CONST: '/project/:vendor/:project/',
+
 	projectDetail: generateLink<'vendor' | 'project'>('/project/:vendor/:project/'),
 })
 
 const ROUTE_ACCORD_LINKS = Object.freeze({
-	accordIndex: '/accord/',
+	accordIndex:        '/accord/',
 	accordDetail_CONST: '/accord/:idcode/',
+
 	accordDetail: generateLink<'idcode'>('/accord/:idcode/'),
 })
 
 const ROUTE_NOKIA_LINKS = Object.freeze({
-	// href: '/nokia/',
-	nokiaIndex: '/nokia/',
-
-	nokiaForm: '/nokia/meeting/',
-	nokiaFormEdit_CONST: '/nokia/meeting/:meetingId/',
-	nokiaFormEdit: generateLink<'meetingId'>('/nokia/meeting/:meetingId/'),
-
-	nokiaPeople: '/nokia/people/',
-	nokiaPeopleForm: '/nokia/people/add/',
+	nokiaIndex:              '/nokia/',
+	nokiaForm:               '/nokia/meeting/',
+	nokiaFormEdit_CONST:     '/nokia/meeting/:meetingId/',
+	nokiaPeople:             '/nokia/people/',
+	nokiaPeopleForm:         '/nokia/people/add/',
 	nokiaPeopleDetail_CONST: '/nokia/people/:personId/',
-	nokiaPeopleDetail: generateLink<'personId'>('/nokia/people/:personId/'),
-	nokiaPeopleEdit_CONST: '/nokia/people/:personId/edit/',
+	nokiaPeopleEdit_CONST:   '/nokia/people/:personId/edit/',
+	nokiaStatistic:          '/nokia/statistic/',
+	nokiaPinarik:            '/nokia/pinarik/',
 
-	nokiaPeopleEdit: generateLink<'personId'>('/nokia/people/:personId/edit/'),
-	nokiaStatistic: '/nokia/statistic/',
-	nokiaPinarik: '/nokia/pinarik/',
+	nokiaFormEdit:     generateLink<'meetingId'>('/nokia/meeting/:meetingId/'),
+	nokiaPeopleDetail: generateLink<'personId'>('/nokia/people/:personId/'),
+	nokiaPeopleEdit:   generateLink<'personId'>('/nokia/people/:personId/edit/'),
 })
 
 const ROUTE_TOURISM_LINKS = Object.freeze({
-	tourismIndex: '/tourism/',
-	tourismMaps: '//tourism/maps/', // TODO: надо название хлебной крошки, надо саму страницу
-	tourismNashe: '/tourism/nashe/',
-	tourismNashe_CONST: '/tourism/nashe/:year/',
-	tourismNashe2017: '/tourism/nashe/2017/',
-	tourismNashe2018: '/tourism/nashe/2018/',
-	tourismWalkDetail: generateLink<'idcode'>('/tourism/walk/:idcode/'),
-	tourismWalkDetail_CONST: '/tourism/walk/:idcode/',
-	tourismChecklist: '/tourism/checklist/',
-	tourismMapsRegion: '/tourism/maps/region/',
+	tourismIndex:                '/tourism/',
+	tourismMaps:                 '/tourism/maps/', // TODO: надо название хлебной крошки, надо саму страницу
+	tourismNashe:                '/tourism/nashe/',
+	tourismNashe_CONST:          '/tourism/nashe/:year/',
+	tourismNashe2017:            '/tourism/nashe/2017/',
+	tourismNashe2018:            '/tourism/nashe/2018/',
+	tourismWalkDetail_CONST:     '/tourism/walk/:idcode/',
+	tourismChecklist:            '/tourism/checklist/',
+	tourismMapsRegion:           '/tourism/maps/region/',
 	tourismMapsMoscowWalkaround: '/tourism/maps/moscow-walkaround/',
-	tourismMapsMoscowBar: '/tourism/maps/moscow-bar/',
-	tourismCityIndex: '/tourism/city/',
-	tourismVisited: '/tourism/visited/',
-	tourismFatherTrack: '/tourism/father-track/',
+	tourismMapsMoscowBar:        '/tourism/maps/moscow-bar/',
+	tourismCityIndex:            '/tourism/city/',
+	tourismVisited:              '/tourism/visited/',
+	tourismFatherTrack:          '/tourism/father-track/',
+
+	tourismWalkDetail: generateLink<'idcode'>('/tourism/walk/:idcode/'),
 })
 
 const ROUTE_LIST_LIST_LINKS = Object.freeze({
-	listListIndex: '/list-list/main/',
+	listListIndex:        '/list-list/main/',
 	listListDetail_CONST: '/list-list/:idcode/',
+	listListCreate:       '/list-list/create/',
+	listListEdit_CONST:   '/list-list/edit/:idcode/',
+
 	listListDetail: generateLink<'idcode'>('/list-list/:idcode/'),
-	listListCreate: '/list-list/create/',
-	listListEdit_CONST: '/list-list/edit/:idcode/',
-	listListEdit: generateLink<'idcode'>('/list-list/edit/:idcode/'),
+	listListEdit:   generateLink<'idcode'>('/list-list/edit/:idcode/'),
 })
 
 const ROUTE_VK_LINKS = Object.freeze({
-	toolVkIndex: '/tool/vk/',
-	toolVkStaticAlbum: '/tool/vk/static-album/',
-	toolVkDownloadAlbum: '/tool/vk/download-album/',
-	toolVkAlbums: '/tool/vk/albums/',
+	toolVkIndex:             '/tool/vk/',
+	toolVkStaticAlbum:       '/tool/vk/static-album/',
+	toolVkDownloadAlbum:     '/tool/vk/download-album/',
+	toolVkAlbums:            '/tool/vk/albums/',
 	toolVkAlbumSingle_CONST: '/tool/vk/albums/:albumId/',
+
 	toolVkAlbumSingle: generateLink<'albumId'>('/tool/vk/albums/:albumId/'),
 })
 
 const ROUTE_PETPROJECT_LINKS = Object.freeze({
-	petProject: '/tool/pet-project/',
-	petProjectCreate: '/tool/pet-project/create/',
+	petProject:           '/tool/pet-project/',
+	petProjectCreate:     '/tool/pet-project/create/',
 	petProjectEdit_CONST: '/tool/pet-project/:projectId/edit/',
 })
 
 const ROUTE_TOOL_LINKS = Object.freeze({
-	toolIndex: '/tool/',
-	toolComoji: '/tool/comoji/',
+	toolIndex:   '/tool/',
+	toolComoji:  '/tool/comoji/',
 	toolEmailer: '/tool/emailer/',
-	toolMagic: '/tool/magic/',
+	toolMagic:   '/tool/magic/',
 
 	...ROUTE_VK_LINKS,
 	...ROUTE_PETPROJECT_LINKS,
@@ -90,9 +92,10 @@ const ROUTE_TOOL_LINKS = Object.freeze({
 })
 
 const ROUTE_CINEMA_LINKS = Object.freeze({
-	cinemaIndex: '/cinema/',
-	cinemaScriptIndex: '/cinema/script/',
+	cinemaIndex:              '/cinema/',
+	cinemaScriptIndex:        '/cinema/script/',
 	cinemaScriptDetail_CONST: '/cinema/script/:idcode/',
+
 	cinemaScriptDetail: generateLink<'idcode'>('/cinema/script/:idcode/'),
 })
 
@@ -106,9 +109,9 @@ const ROUTE_RESUME_LINKS = Object.freeze({
 })
 
 const ROUTE_THINGS_LINKS = Object.freeze({
-	thingsIndex: '/things/',
+	thingsIndex:    '/things/',
 	thingsNotebook: '/things/notebook/',
-	thingsVelo: '/things/velo/',
+	thingsVelo:     '/things/velo/',
 })
 
 // Инфостраницы о себе
@@ -118,31 +121,34 @@ const ROUTE_ABOUT_ME_LINKS = Object.freeze({
 	...ROUTE_CINEMA_LINKS,
 
 	gameIndex: '/game/',
-	igLink: '/ig-link/',
+	igLink:    '/ig-link/',
 })
 
 // Делюсь знаниями
 const ROUTE_SHARE_KNOWLEDGE_LINKS = Object.freeze({
-	learningIndex: '/obuchenie/',
+	learningIndex:        '/obuchenie/',
 	learningDetail_CONST: '/obuchenie/:idcode/',
+
 	learningDetail: generateLink<'idcode'>('/obuchenie/:idcode/'),
 })
 
 // Инфосервисы
 const ROUTE_INFO_SERVICE_LINKS = Object.freeze({
 	...ROUTE_SHARE_KNOWLEDGE_LINKS,
-	logism: '/logism/',
-	demagog: '/demagog/',
-	faqIndex: '/faq/',
+
+	logism:          '/logism/',
+	demagog:         '/demagog/',
+	faqIndex:        '/faq/',
 	faqDetail_CONST: '/faq/:idcode/',
+
 	faqDetail: generateLink<'idcode'>('/faq/:idcode/'),
 })
 
 // Сервисы-развлекухи
 const ROUTE_FUNNY_LINKS = Object.freeze({
-	pron: '/pron/',
+	pron:    '/pron/',
 	youtube: '/youtube/',
-	links: '/links/',
+	links:   '/links/',
 })
 
 const ROUTE_LIBRARY_LINKS = Object.freeze({
@@ -156,7 +162,7 @@ const ROUTE_SERVICE_ABOUT_ME_LINKS = Object.freeze({
 })
 
 export const ROUTE_LINKS = {
-	index: '/',
+	index:        '/',
 	serviceIndex: '/service',
 
 	...ROUTE_PROJECT_LINKS,
@@ -170,10 +176,9 @@ export const ROUTE_LINKS = {
 	...ROUTE_FUNNY_LINKS,
 
 	adminIndex: '/admin/',
-	login: '/login/',
-	typo: '/typo/',
-	TODO: '#',
-	TODO_VATRIKOVSKY_SCHOOL: '#',
+	login:      '/login/',
+	typo:       '/typo/',
+	TODO:       '#',
 } as const
 
 export type RouteLinksType = keyof typeof ROUTE_LINKS
