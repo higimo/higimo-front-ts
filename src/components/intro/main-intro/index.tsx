7ede09c6cc920198160d0a565e094fd16e3d8fd2
Дмитрий Уткин <higimo@gmail.com>
2026-05-06 16:11:37 +0300

    Добавил ссылку на портфолио

--- src/components/intro/main-intro/index.tsx
+++ src/components/intro/main-intro/index.tsx
@@ -16,9 +16,8 @@ export const MainIntro: FunctionComponent = () => (
 		<TextContainer>
 			<p class="logo">ХИГИ́МО</p>
 			<p>
-				<a href={ROUTE_LINKS.resumeProduct}>Резюме</a>
+				<a href={ROUTE_LINKS.resumeProduct}>Резюме</a> и <a href={ROUTE_LINKS.projectIndex}>кейсы в портфолио</a>
 			</p>
-			{/* TODO: [LIGHT] поставить ссылки на своё портфолио */}
 			<p>
 				Руководил продуктами <span class="nowrap">в <a href={EXTERNAL_LINKS.kidguru}><CompanyLogo name="kidguru" /> Кидгу.ру</a></span>, где запустил маркетплейс с нуля, <span class="nowrap">и в <a href={EXTERNAL_LINKS.superjob}><CompanyLogo name="SJ" /> Суперджобе</a></span>, отвечал за соискательскую часть и ML‑ранжирование для многомиллионной аудитории. Раньше 10 лет занимался разработкой <span class="nowrap">в <a href={EXTERNAL_LINKS.afishaDaily}><CompanyLogo name="Афиша Daily" /> Афише <em>Дейли</em></a></span>, <a href={EXTERNAL_LINKS.alsHigimo} class="nowrap"><CompanyLogo name="ALS" /> Студии Лебедева</a> <span class="nowrap">и <a href={EXTERNAL_LINKS.rTop}><CompanyLogo name="R-top" /> Эртопе</a></span>.
 			</p>
