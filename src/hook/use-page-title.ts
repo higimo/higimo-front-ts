41948d6b9201806495d7042e095b55c60b5da106
Дмитрий Уткин <higimo@gmail.com>
2026-05-06 18:28:44 +0300

    useTitle может брать данные из словаря хлебных крошек

--- src/hook/use-page-title.ts
+++ src/hook/use-page-title.ts
@@ -1,15 +1,22 @@
+import { BREADCRUMS_DIC, isRouteType } from 'dic/router'
+import { useRoute } from 'preact-iso'
 import { useEffect } from 'preact/hooks'
 
 export const SITE_POSTFIX = 'Хигимо'
 
-// TODO: [LIGHT] вот бы он пробовал взять заголовок из src\dic\router.ts
+const normaliseUrl = (path: string) => {
+	return path + (path[path.length - 1] === '/' ? '' : '/')
+}
 
 /**
  * Хук для управления заголовком страницы
  */
 export const usePageTitle = (title: string | string[], fallback?: string) => {
-	// TODO: [LIGHT] часто оставляет прежний или ещё что-то не делает, что показывается не правильный вариант
+	const { path } = useRoute()
+
 	useEffect(() => {
+		const route = normaliseUrl(path)
+
 		if (Array.isArray(title)) {
 			// TODO: [LIGHT] use concat array
 			document.title = [...title, SITE_POSTFIX].join(' | ')
@@ -17,8 +24,10 @@ export const usePageTitle = (title: string | string[], fallback?: string) => {
 			document.title = title + ' | ' + SITE_POSTFIX
 		} else if (fallback) {
 			document.title = fallback + ' | ' + SITE_POSTFIX
+		} else if (isRouteType(route) && BREADCRUMS_DIC[route]) {
+			document.title = BREADCRUMS_DIC[route] + ' | ' + SITE_POSTFIX
 		} else {
 			document.title = SITE_POSTFIX
 		}
-	}, [title, fallback])
+	}, [title, fallback, path])
 }
