c955e6ede15b8de6d7d236837beab92d9a486044
Дмитрий Уткин <higimo@gmail.com>
2026-05-09 18:05:54 +0300

    Оформление страницы

--- src/components/project/project-viewer/index.tsx
+++ src/components/project/project-viewer/index.tsx
@@ -1,10 +1,6 @@
 import { FunctionComponent } from 'preact'
+import { PortfolioProjectDetailType } from 'api-types/portfolio.types'
 
-import { useRoute } from 'preact-iso'
-import { useProjectViewer } from 'components/project/hooks/useProjectViewer'
-import { usePageTitle } from 'hook/use-page-title'
-
-import { Loading } from 'components/ui/loading'
 import { OnlyAdmin } from 'components/util/only-admin'
 import { PortfolioCreditsGallery } from 'components/project/portfolio-credits-gallery'
 import { PortfolioTagsGallery } from 'components/project/portfolio-tags-gallery'
@@ -14,49 +10,33 @@ import { WorkerInput } from 'components/form/project/worker-input'
 import { getHumanDate } from 'components/project/utils/getHumanDate'
 import { getProjectText } from 'components/project/utils/getProjectText'
 
-import { NotFoundPage } from 'pages/not-found-page'
-
 import './style.css'
 
-export const ProjectViewer: FunctionComponent = () => {
-	const { params: { vendor, project } } = useRoute()
-
-	const [curProject, isLoading, isEmpty] = useProjectViewer(vendor, project)
-
-	usePageTitle(curProject.name ? `${curProject.name} | Проект Хигимо` : 'Проект Хигимо')
-
-	if (isLoading) {
-		return <Loading />
-	}
-	if (isEmpty) {
-		return <NotFoundPage />
-	}
-
-	const { date, name, text = '' } = curProject
-
-	return (
-		<div className="project-viewer">
-			<TextContainer className="project-viewer__date">
-				{getHumanDate(date)}
-			</TextContainer>
-			<TextContainer>
-				<h1>{name}</h1>
-			</TextContainer>
-			<div
-				className="content"
-				dangerouslySetInnerHTML={{
-					__html: getProjectText(text)
-				}}
-			/>
-			<OnlyAdmin>
-				<div>
-					<WorkerInput projectId={curProject.id} />
-				</div>
-			</OnlyAdmin>
-			<PortfolioCreditsGallery credits={curProject.credits} />
-			{/* TODO: [MEDIUM] TAGS применить мапинг категоризации тегов */}
-			{/* TODO: [HARD] Сделать интерфейс фигули, которая прописывает теги */}
-			<PortfolioTagsGallery tags={curProject.tags || []} />
-		</div>
-	)
+type ProjectViewerPropsType = {
+	project: PortfolioProjectDetailType
 }
+export const ProjectViewer: FunctionComponent<ProjectViewerPropsType> = ({ project }) => (
+	<div className="project-viewer">
+		<TextContainer className="project-viewer__date">
+			{getHumanDate(project.date)}
+		</TextContainer>
+		<TextContainer>
+			<h1>{project.name}</h1>
+		</TextContainer>
+		<div
+			className="content"
+			dangerouslySetInnerHTML={{
+				__html: getProjectText(project.text)
+			}}
+		/>
+		<OnlyAdmin>
+			<div>
+				<WorkerInput projectId={project.id} />
+			</div>
+		</OnlyAdmin>
+		<PortfolioCreditsGallery credits={project.credits} />
+		{/* TODO: [MEDIUM] TAGS применить мапинг категоризации тегов */}
+		{/* TODO: [HARD] Сделать интерфейс фигули, которая прописывает теги */}
+		<PortfolioTagsGallery tags={project.tags || []} />
+	</div>
+)
--- src/pages/project/project-single-page/index.tsx
+++ src/pages/project/project-single-page/index.tsx
@@ -1,9 +1,29 @@
 import { FunctionComponent } from 'preact'
-import { ProjectViewer } from 'components/project/project-viewer'
 
-// TODO: [LIGHT] совершенно лишний компонент, сразу кидать вьювер
+import { useProjectViewer } from 'components/project/hooks/useProjectViewer'
+import { useRoute } from 'preact-iso'
+import { usePageTitle } from 'hook/use-page-title'
+
+import { ProjectViewer } from 'components/project/project-viewer'
+import { Loading } from 'components/ui/loading'
+import { NotFoundData } from 'components/ui/not-found-data'
+
+
 export const ProjectSinglePage: FunctionComponent = () => {
+	const { params: { vendor, project } } = useRoute()
+
+	const [curProject, isLoading, isEmpty] = useProjectViewer(vendor, project)
+
+	usePageTitle(curProject.name ? `${curProject.name} | Проект Хигимо` : 'Проект Хигимо')
+
+	if (isLoading) {
+		return <Loading />
+	}
+	if (isEmpty) {
+		return <NotFoundData />
+	}
+
 	return (
-		<ProjectViewer />
+		<ProjectViewer project={curProject} />
 	)
 }
