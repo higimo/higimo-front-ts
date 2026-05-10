69709b98b87d5d6c6e4874cc3207a77fc3053f89
Дмитрий Уткин <higimo@gmail.com>
2026-05-10 10:52:10 +0300

    Неиспользуемые функции

--- src/utils/url-route/component-url-formatter.ts
+++
@@ -1,8 +0,0 @@
-type ConfigType = {
-	urlKey: string
-	mask: string
-}
-type ComponentUrlFormatterType = (config: ConfigType) => (code: string) => string
-export const componentUrlFormatter: ComponentUrlFormatterType = config => code => {
-	return config.mask.replace(`%${config.urlKey}%`, code)
-}
--- src/utils/url-route/url-formatter-router.ts
+++
@@ -1,3 +0,0 @@
-export const urlFormatterRouter = (url: string) => (code: string) => {
-	return url.replace(/\/:([^\/]+)/, (_, key) => `/${code[key]}`)
-}
