6b54c074484955dae9e6d19e3b323851ba5f44fb
Дмитрий Уткин <higimo@gmail.com>
2026-08-17 22:25:26 +0300

    Хлебные крошки, исправил ещё баг подбора названия

--- src/components/ui/breadcrumps/index.tsx
+++ src/components/ui/breadcrumps/index.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from 'preact'
 
+import { useMemo } from 'preact/hooks'
 import { useRoute } from 'preact-iso'
 
 import { MaybeLink } from 'components/ui/maybe-link'
@@ -14,20 +15,34 @@ import './style.css'
 export const Breadcrumps: FunctionComponent = () => {
 	const { path } = useRoute()
 
-	const breadcrumbs = convertPathToUrl(path)
-	const breadcrumbsLength = breadcrumbs.length - 1
+	const breadcrumbs = useMemo(() => {
+		const breadcrumbPaths = convertPathToUrl(path)
+		const length = breadcrumbPaths.length - 1
+
+		return breadcrumbPaths.map((path, index) => {
+			const isLastChild = index === length
+			const routeName = isRouteType(path) ? BREADCRUMS_DIC[path] : undefined
+			const localName = !!routeName ? routeName : getPageTitle()
+			return {
+				title: localName,
+				href: path,
+				isLastChild,
+			}
+		})
+	}, [path, getPageTitle()])
+
 	return (
 		<div className="breadcrumbs">
-			{breadcrumbs.map((breadcrumb, index) => {
-				const isLastChild = index === breadcrumbsLength
-				const routeName = isRouteType(breadcrumb) ? BREADCRUMS_DIC[breadcrumb] : undefined
-				const newLocal = isLastChild ? getPageTitle() : routeName
-				return (
-					<div className="breadcrumbs__item">
-						<MaybeLink href={breadcrumb} isHref={!isLastChild}>{newLocal}</MaybeLink>
-					</div>
-				)
-			})}
+			{breadcrumbs.map((breadcrumb) => (
+				<div className="breadcrumbs__item">
+					<MaybeLink
+						href={breadcrumb.href}
+						isHref={!breadcrumb.isLastChild}
+					>
+						{breadcrumb.title}
+					</MaybeLink>
+				</div>
+			))}
 		</div>
 	)
 }
