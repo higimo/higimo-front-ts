8aa6180dfe1bf7095f776f26a3acb1221cdfc76f
Дмитрий Уткин <higimo@gmail.com>
2026-09-09 00:29:35 +0300

    Тут такое не надо

--- src/components/intro/travel-invite/index.tsx
+++ src/components/intro/travel-invite/index.tsx
@@ -3,8 +3,8 @@ import { FunctionComponent } from 'preact'
 import { PrecentationContainer } from 'components/ui/precentation-container'
 import { TextContainer } from 'components/ui/text-container'
 
-import { ROUTE_LINKS } from 'dic/ROUTE_LINKS'
 import { ANCHOR_LINKS } from 'dic/ANCHOR_LINKS'
+import { ROUTE_LINKS } from 'dic/ROUTE_LINKS'
 import { tourismMenuLinks } from 'components/tourism/tourism-main-menu/data'
 
 import './style.css'
@@ -13,7 +13,6 @@ export const TravelInvite: FunctionComponent = () => {
 	return (
 		<PrecentationContainer className="travel-invite" id={ANCHOR_LINKS.travel}>
 			<TextContainer>
-				{/* TODO: [LIGHT] tourism header */}
 				<h2 className="travel-invite__title">
 					<a href={ROUTE_LINKS.tourismIndex}>Путешествую</a><sup>150 населённых пунктов</sup>
 				</h2>
