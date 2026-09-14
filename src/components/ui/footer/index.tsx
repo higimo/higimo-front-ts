55b468dcbb1b697ea023e1f5241cfb8d5e516ea8
Дмитрий Уткин <higimo@gmail.com>
2026-09-15 02:48:40 +0300

    Типизация в футере

--- src/components/ui/footer/index.tsx
+++ src/components/ui/footer/index.tsx
@@ -1,28 +1,26 @@
 import { FunctionComponent } from 'preact'
+import { ToolDataType } from 'utils.type'
 
 import { useAuth } from 'hook/fetch/use-auth'
 import { useGlobalContext } from 'context/global'
 
-import { contactListData } from 'dic/intra-links/contact-list'
-import { blogInviteData } from 'dic/intra-links/blog-invite'
-import { shareKnowledgeData } from 'dic/intra-links/share-knowledge'
 import { aboutInviteList } from 'dic/intra-links/about-invite'
-import { toolListData } from 'dic/intra-links/tools-intro'
+import { blogInviteData } from 'dic/intra-links/blog-invite'
+import { contactListData } from 'dic/intra-links/contact-list'
 import { funnyList } from 'dic/intra-links/funny-invite'
+import { shareKnowledgeData } from 'dic/intra-links/share-knowledge'
+import { toolListData } from 'dic/intra-links/tools-intro'
 
 import { ROUTE_LINKS } from 'dic/ROUTE_LINKS'
 
 import './style.css'
 
-const SLICE_ABOUT = 6
-const SLICE_TOOL = 3
-
-const renderLink = (isAuth: boolean) => (toolElement) => {
-	if (!!toolElement.isAdmin && !isAuth || !!toolElement.isArhive) {
+const renderLink = (isAuth: boolean) => (toolElement: ToolDataType) => {
+	if (!!toolElement.isAdmin && !isAuth || !!toolElement.isArchive) {
 		return null
 	}
 	return (
-		<div className="footer__link"><a href={toolElement.link as string}>{toolElement.name}</a></div>
+		<div className="footer__link"><a href={toolElement.href as string}>{toolElement.name}</a></div>
 	)
 }
 
@@ -40,7 +38,6 @@ export const Footer: FunctionComponent = () => {
 				<div className="footer__header"><a href={ROUTE_LINKS.projectIndex}>Сделал</a></div>
 				<div className="footer__header">Связаться</div>
 				{contactListData.map(renderLink(isAuth))}
-				{/* TODO: [LIGHT] добавить флоу из глаз */}
 				<div className="footer__header">Блоги</div>
 				{blogInviteData.map(renderLink(isAuth))}
 			</div>
@@ -62,6 +59,7 @@ export const Footer: FunctionComponent = () => {
 				{toolListData.map(renderLink(isAuth))}
 			</div>
 			<div className="footer__copyright">
+				{/* TODO: заменить на svg, чтоб везде было видно */}
 				Сделал Хиги́мо с гордостью в 🇷🇺
 			</div>
 		</footer>
