2e8a384010dcaa7a0f1d296a3a63a1564d6f4636
Дмитрий Уткин <higimo@gmail.com>
2026-09-23 02:29:32 +0300

    Отклики. Добавил туту-список

---
+++ src/components/hiring-response/hiring-response-todo/pasteApi.tsx
@@ -0,0 +1,43 @@
+import { PasteApiType } from 'api-types/paste.types';
+import { API_ROUTE } from 'dic/API_ROUTE';
+import { sendRequest } from 'utils/api/send-request';
+
+class PasteApiService {
+	async getByKey(key: PasteApiType['key']): Promise<PasteApiType[] | null> {
+		try {
+			const data = await sendRequest<PasteApiType[]>(API_ROUTE.pasteSingle({ id: key }));
+			return data.data;
+		} catch (error) {
+			// TODO: что-то получше бы предоставить, тост хотя бы
+			console.error(error);
+			return null;
+		}
+	}
+
+	async create(values: Partial<Omit<PasteApiType, 'id'>>): Promise<PasteApiType | null> {
+		try {
+			const data = await sendRequest<PasteApiType>(API_ROUTE.paste, {
+				method: 'POST',
+				values: values
+			});
+			return data.data;
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	}
+
+	async edit(values: Partial<PasteApiType>): Promise<PasteApiType | null> {
+		try {
+			const data = await sendRequest<PasteApiType>(API_ROUTE.pasteSingle({ id: values.id?.toString() || '' }), {
+				method: 'PUT',
+				values: values
+			});
+			return data.data;
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	}
+}
+export const pasteApi = new PasteApiService();
